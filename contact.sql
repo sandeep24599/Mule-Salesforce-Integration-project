6a56f46b5d00e253c4e783f7b615a793fd0bf5e5
Sandeep Bondalapati <85096875+sandeep24599@users.noreply.github.com>
2024-05-14 12:53:31 -0400

    Add files via upload
    Initial commit

---
+++ contact.sql
@@ -0,0 +1,712 @@
+create database contact
+
+use contact
+
+/* CREATE TABLE */
+CREATE TABLE contacts (
+`FirstName` text,
+`LastName` text,
+`Email` text,
+`Company` text
+);
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Kazu', 'mwagstaffe0@npr.org', 'Maura', 'Wagstaffe'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Skibox', 'meliet1@buzzfeed.com', 'Myrwyn', 'Eliet'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Riffwire', 'bblakden2@goo.ne.jp', 'Briney', 'Blakden'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Babblestorm', 'dfurmagier3@apache.org', 'Denny', 'Furmagier'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Twitterwire', 'foade4@oaic.gov.au', 'Flossie', 'Oade'
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Eire', 'smcilenna5@unblog.fr', 'Shanda', 'McIlenna'
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Twitterwire', 'vcurrum6@github.io', 'Vernen', 'Currum'
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Tanoodle', 'egoublier7@netvibes.com', 'Enoch', 'Goublier'
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Mydo', 'lhowie8@omniture.com', 'Lottie', 'Howie'
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Avavee', 'vanselm9@slashdot.org', 'Vivi', 'Anselm'
+);
+
+/* INSERT QUERY NO: 11 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Browsetype', 'mconklina@about.com', 'Merle', 'Conklin'
+);
+
+/* INSERT QUERY NO: 12 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Oloo', 'mjoseyb@vkontakte.ru', 'Matthieu', 'Josey'
+);
+
+/* INSERT QUERY NO: 13 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Aibox', 'cortonc@gmpg.org', 'Corty', 'Orton'
+);
+
+/* INSERT QUERY NO: 14 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Youopia', 'pkleinertd@imdb.com', 'Pauly', 'Kleinert'
+);
+
+/* INSERT QUERY NO: 15 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Blogtags', 'bvearnalse@hao123.com', 'Ban', 'Vearnals'
+);
+
+/* INSERT QUERY NO: 16 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Gabspot', 'stelferf@usnews.com', 'Suzanna', 'Telfer'
+);
+
+/* INSERT QUERY NO: 17 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Katz', 'abeavesg@abc.net.au', 'Allyn', 'Beaves'
+);
+
+/* INSERT QUERY NO: 18 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Tagchat', 'tvalentellih@bandcamp.com', 'Thaddeus', 'Valentelli'
+);
+
+/* INSERT QUERY NO: 19 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Trudoo', 'rkrinksi@pbs.org', 'Ricky', 'Krinks'
+);
+
+/* INSERT QUERY NO: 20 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Gabtune', 'kbainbriggej@psu.edu', 'Kori', 'Bainbrigge'
+);
+
+/* INSERT QUERY NO: 21 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Photospace', 'wocahsedyk@purevolume.com', 'Waylen', 'Cahsedy'
+);
+
+/* INSERT QUERY NO: 22 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Janyx', 'vhowenl@blogtalkradio.com', 'Venus', 'Howen'
+);
+
+/* INSERT QUERY NO: 23 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Brightdog', 'hhuonicm@java.com', 'Horace', 'Huonic'
+);
+
+/* INSERT QUERY NO: 24 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Innojam', 'gbuckwelln@abc.net.au', 'Grady', 'Buckwell'
+);
+
+/* INSERT QUERY NO: 25 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Omba', 'rpollako@ustream.tv', 'Rori', 'Pollak'
+);
+
+/* INSERT QUERY NO: 26 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Quatz', 'mpeppettp@bigcartel.com', 'Margaretta', 'Peppett'
+);
+
+/* INSERT QUERY NO: 27 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Yoveo', 'pbowgenq@myspace.com', 'Pierce', 'Bowgen'
+);
+
+/* INSERT QUERY NO: 28 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Yamia', 'stanseyr@angelfire.com', 'Shepperd', 'Tansey'
+);
+
+/* INSERT QUERY NO: 29 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Zoomlounge', 'mlorroways@msn.com', 'Mirna', 'Lorroway'
+);
+
+/* INSERT QUERY NO: 30 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Cogidoo', 'mgrelkat@mtv.com', 'Matthieu', 'Grelka'
+);
+
+/* INSERT QUERY NO: 31 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Thoughtmix', 'dtreatu@over-blog.com', 'Dulcia', 'Treat'
+);
+
+/* INSERT QUERY NO: 32 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Leenti', 'rmartusovv@netscape.com', 'Robinet', 'Martusov'
+);
+
+/* INSERT QUERY NO: 33 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Centizu', 'kfarrisw@arstechnica.com', 'Karalee', 'Farris'
+);
+
+/* INSERT QUERY NO: 34 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Meeveo', 'hwillshirex@google.de', 'Hallsy', 'Willshire'
+);
+
+/* INSERT QUERY NO: 35 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Brainlounge', 'hfenichy@princeton.edu', 'Hewie', 'Fenich'
+);
+
+/* INSERT QUERY NO: 36 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Skinix', 'swaldenz@ucla.edu', 'Shauna', 'Walden'
+);
+
+/* INSERT QUERY NO: 37 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Bubblebox', 'kmusgrave10@goo.ne.jp', 'Kassey', 'Musgrave'
+);
+
+/* INSERT QUERY NO: 38 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Edgepulse', 'braistrick11@ox.ac.uk', 'Burnaby', 'Raistrick'
+);
+
+/* INSERT QUERY NO: 39 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Jaloo', 'egoman12@netvibes.com', 'Errol', 'Goman'
+);
+
+/* INSERT QUERY NO: 40 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Tambee', 'jpleven13@biglobe.ne.jp', 'Jania', 'Pleven'
+);
+
+/* INSERT QUERY NO: 41 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Linkbridge', 'mharryman14@businesswire.com', 'Matt', 'Harryman'
+);
+
+/* INSERT QUERY NO: 42 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Gabvine', 'kbulled15@imgur.com', 'Karl', 'Bulled'
+);
+
+/* INSERT QUERY NO: 43 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Camimbo', 'bgracewood16@yellowbook.com', 'Beverly', 'Gracewood'
+);
+
+/* INSERT QUERY NO: 44 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Innojam', 'dtimmermann17@studiopress.com', 'Donnell', 'Timmermann'
+);
+
+/* INSERT QUERY NO: 45 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Aimbo', 'torrill18@stumbleupon.com', 'Towney', 'Orrill'
+);
+
+/* INSERT QUERY NO: 46 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Vipe', 'rpirot19@go.com', 'Ralph', 'Pirot'
+);
+
+/* INSERT QUERY NO: 47 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Lazz', 'asowerbutts1a@a8.net', 'Aldus', 'Sowerbutts'
+);
+
+/* INSERT QUERY NO: 48 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Blogtags', 'dsynder1b@hhs.gov', 'Danna', 'Synder'
+);
+
+/* INSERT QUERY NO: 49 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Meejo', 'jaspinwall1c@reddit.com', 'Janek', 'Aspinwall'
+);
+
+/* INSERT QUERY NO: 50 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Tazzy', 'dcarine1d@ca.gov', 'Desirae', 'Carine'
+);
+
+/* INSERT QUERY NO: 51 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Blognation', 'gpresman1e@hhs.gov', 'Garnette', 'Presman'
+);
+
+/* INSERT QUERY NO: 52 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Eadel', 'mwonfar1f@cbsnews.com', 'Madelena', 'Wonfar'
+);
+
+/* INSERT QUERY NO: 53 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Wikizz', 'fwhitecross1g@netvibes.com', 'Francoise', 'Whitecross'
+);
+
+/* INSERT QUERY NO: 54 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Edgepulse', 'cmackellen1h@uiuc.edu', 'Carol', 'MacKellen'
+);
+
+/* INSERT QUERY NO: 55 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Jaxworks', 'mlegall1i@ted.com', 'Marcelo', 'Le Gall'
+);
+
+/* INSERT QUERY NO: 56 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Einti', 'cmilella1j@sun.com', 'Christie', 'Milella'
+);
+
+/* INSERT QUERY NO: 57 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Thoughtworks', 'edewerk1k@hud.gov', 'Elizabeth', 'de Werk'
+);
+
+/* INSERT QUERY NO: 58 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Yodo', 'dvasyatkin1l@nydailynews.com', 'Daryl', 'Vasyatkin'
+);
+
+/* INSERT QUERY NO: 59 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Kazio', 'vmcelrea1m@jugem.jp', 'Vale', 'McElrea'
+);
+
+/* INSERT QUERY NO: 60 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Oyoyo', 'walgate1n@boston.com', 'Wayland', 'Algate'
+);
+
+/* INSERT QUERY NO: 61 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Flashset', 'vdominique1o@google.es', 'Verney', 'Dominique'
+);
+
+/* INSERT QUERY NO: 62 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Zoonder', 'vallardyce1p@npr.org', 'Virgilio', 'Allardyce'
+);
+
+/* INSERT QUERY NO: 63 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Cogibox', 'sbonnin1q@cdbaby.com', 'Sib', 'Bonnin'
+);
+
+/* INSERT QUERY NO: 64 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Latz', 'rfaers1r@cyberchimps.com', 'Rochester', 'Faers'
+);
+
+/* INSERT QUERY NO: 65 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Aivee', 'zhailston1s@eventbrite.com', 'Zak', 'Hailston'
+);
+
+/* INSERT QUERY NO: 66 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Twimm', 'ethyer1t@arstechnica.com', 'Ezekiel', 'Thyer'
+);
+
+/* INSERT QUERY NO: 67 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'DabZ', 'aclarage1u@unc.edu', 'Annice', 'Clarage'
+);
+
+/* INSERT QUERY NO: 68 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Eidel', 'mchattey1v@tinypic.com', 'Mauricio', 'Chattey'
+);
+
+/* INSERT QUERY NO: 69 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Oyoloo', 'ysimester1w@google.fr', 'Yetta', 'Simester'
+);
+
+/* INSERT QUERY NO: 70 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Wordware', 'smcentegart1x@hatena.ne.jp', 'Sela', 'McEntegart'
+);
+
+/* INSERT QUERY NO: 71 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Flashpoint', 'ajedrachowicz1y@live.com', 'Annabel', 'Jedrachowicz'
+);
+
+/* INSERT QUERY NO: 72 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Pixope', 'mguilloneau1z@umn.edu', 'Mariejeanne', 'Guilloneau'
+);
+
+/* INSERT QUERY NO: 73 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Fiveclub', 'pbartelsellis20@technorati.com', 'Prudence', 'Bartels-Ellis'
+);
+
+/* INSERT QUERY NO: 74 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Gabspot', 'lcowlin21@newsvine.com', 'Lettie', 'Cowlin'
+);
+
+/* INSERT QUERY NO: 75 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Tazzy', 'rnoyce22@vk.com', 'Robinetta', 'Noyce'
+);
+
+/* INSERT QUERY NO: 76 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Browsetype', 'fteggart23@diigo.com', 'Fabe', 'Teggart'
+);
+
+/* INSERT QUERY NO: 77 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Yodoo', 'jkenen24@reference.com', 'Josey', 'Kenen'
+);
+
+/* INSERT QUERY NO: 78 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Abatz', 'aatwel25@histats.com', 'Alfy', 'Atwel'
+);
+
+/* INSERT QUERY NO: 79 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Zoombeat', 'beastabrook26@princeton.edu', 'Billie', 'Eastabrook'
+);
+
+/* INSERT QUERY NO: 80 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Wikido', 'tlabin27@eepurl.com', 'Thebault', 'Labin'
+);
+
+/* INSERT QUERY NO: 81 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Feedfish', 'maberkirdo28@ihg.com', 'Marcile', 'Aberkirdo'
+);
+
+/* INSERT QUERY NO: 82 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Plajo', 'yrichly29@paypal.com', 'Yasmin', 'Richly'
+);
+
+/* INSERT QUERY NO: 83 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Blogspan', 'ffollitt2a@xrea.com', 'Flory', 'Follitt'
+);
+
+/* INSERT QUERY NO: 84 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Yozio', 'mvaadeland2b@surveymonkey.com', 'Myrtia', 'Vaadeland'
+);
+
+/* INSERT QUERY NO: 85 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Jaxnation', 'rfryatt2c@tinypic.com', 'Rosemary', 'Fryatt'
+);
+
+/* INSERT QUERY NO: 86 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Photospace', 'rdearness2d@google.com.hk', 'Reamonn', 'Dearness'
+);
+
+/* INSERT QUERY NO: 87 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Youbridge', 'aleteve2e@craigslist.org', 'Annie', 'Le Teve'
+);
+
+/* INSERT QUERY NO: 88 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Izio', 'hcuerdall2f@xrea.com', 'Hagen', 'Cuerdall'
+);
+
+/* INSERT QUERY NO: 89 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Dynazzy', 'btroker2g@shareasale.com', 'Bonnee', 'Troker'
+);
+
+/* INSERT QUERY NO: 90 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Devify', 'fainger2h@hubpages.com', 'Finlay', 'Ainger'
+);
+
+/* INSERT QUERY NO: 91 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Gabvine', 'dbeddie2i@spotify.com', 'Dulcinea', 'Beddie'
+);
+
+/* INSERT QUERY NO: 92 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Realblab', 'tsokell2j@cmu.edu', 'Tamarra', 'Sokell'
+);
+
+/* INSERT QUERY NO: 93 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Youtags', 'fcasper2k@bravesites.com', 'Federica', 'Casper'
+);
+
+/* INSERT QUERY NO: 94 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Skinder', 'lmackill2l@studiopress.com', 'Lynnelle', 'MacKill'
+);
+
+/* INSERT QUERY NO: 95 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Youspan', 'tsallenger2m@domainmarket.com', 'Thorpe', 'Sallenger'
+);
+
+/* INSERT QUERY NO: 96 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Thoughtbeat', 'afinnick2n@comsenz.com', 'Amaleta', 'Finnick'
+);
+
+/* INSERT QUERY NO: 97 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Cogilith', 'tfleckno2o@163.com', 'Tedi', 'Fleckno'
+);
+
+/* INSERT QUERY NO: 98 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Edgewire', 'acommusso2p@examiner.com', 'Ardeen', 'Commusso'
+);
+
+/* INSERT QUERY NO: 99 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Photolist', 'tyardley2q@cisco.com', 'Tadeas', 'Yardley'
+);
+
+/* INSERT QUERY NO: 100 */
+INSERT INTO contacts(Company, Email, FirstName, LastName)
+VALUES
+(
+'Dabvine', 'rkrzysztofiak2r@i2i.jp', 'Roley', 'Krzysztofiak'
+);
+
